e597afe0a09fc22d84efb14419522e37ba51524c
Orhan-Veli <orhan_1576@hotmail.com>
2022-06-27 23:21:42 +0300

    Facade

---
+++ Facade/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Drawing;
+using System;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShapeMaker shapeMaker = new ShapeMaker();
+
+            shapeMaker.DrawCircle();
+            shapeMaker.DrawRectangle();
+            shapeMaker.DrawSquare();
+        }
+    }
+
+    public interface Shape
+    {
+        void Draw();
+    }
+
+    public class Rectangle : Shape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Rectangle Draw");
+        }
+    }
+
+    public class Square : Shape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Square Draw");
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Circle Draw");
+        }
+    }
+
+    public class ShapeMaker
+    {
+        private Shape circle;
+        private Shape rectangle;
+        private Shape square;
+
+        public ShapeMaker()
+        {
+            circle = new Circle();
+            rectangle = new Rectangle();
+            square = new Square();
+        }
+
+        public void DrawCircle()
+        {
+            circle.Draw();
+        }
+
+        public void DrawRectangle()
+        {
+            rectangle.Draw();
+        }
+
+        public void DrawSquare()
+        {
+            square.Draw();
+        }
+    }
+}
