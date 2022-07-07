94b59ba28aa812023d394a3e036a54b8c4474a53
Orhan-Veli <orhan_1576@hotmail.com>
2022-07-07 22:52:25 +0300

    Mediator

---
+++ Mediator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User robert = new User("Robert");
+            User john = new User("John");
+
+            robert.SendMessage("Hi! John!");
+            john.SendMessage("Hello! Robert!");
+        }
+    }
+
+    public class ChatRoom
+    {
+        public static void SendMessage(User user, string message)
+        {
+            Console.WriteLine(" [" + user.Name + "] : " + message);
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        
+        public User(string name)
+        {
+            Name = name;
+        }
+
+        public void SendMessage(string message)
+        {
+            ChatRoom.SendMessage(this,message);
+        }
+    }
+}
