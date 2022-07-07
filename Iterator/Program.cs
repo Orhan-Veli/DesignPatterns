18d4f3a2a12a64206562052fffabb3ebc7312f49
Orhan-Veli <orhan_1576@hotmail.com>
2022-07-07 22:38:44 +0300

    Iterator

---
+++ Iterator/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           ConcreteAggregate a = new ConcreteAggregate();
+
+           a[0] = "Item A";
+           a[1] = "Item B";
+           a[2] = "Item C";
+           a[3] = "Item D";
+
+           Iterator i = a.CreateIterator();
+
+           Console.WriteLine("Iterating over collection:");
+
+           object item = i.First();
+           while (item != null)
+           {
+            Console.WriteLine(item);
+            item = i.Next();
+           }
+
+           Console.ReadKey();
+        }
+    }
+
+    public abstract class Aggregate
+    {
+        public abstract Iterator CreateIterator();
+    }
+
+    public class ConcreteAggregate : Aggregate
+    {
+        public List<object> items = new List<object>();
+
+        public override Iterator CreateIterator()
+        {
+            return new ConcreteIterator(this);
+        }
+
+        public int count
+        {
+            get { return items.Count; }
+        }
+
+        public object this[int index]
+        {
+            get{return items[index];}
+            set{items.Insert(index,value);}
+        }
+    }
+    
+    public abstract class Iterator
+    {
+        public abstract object First();
+        public abstract object Next();
+        public abstract bool IsDone();
+        public abstract object CurrentItem();
+    }
+
+    public class ConcreteIterator : Iterator
+    {
+        ConcreteAggregate aggregate;
+        int current = 0;
+
+        public ConcreteIterator(ConcreteAggregate aggregate)
+        {
+            this.aggregate = aggregate;
+        }
+
+        public override object First()
+        {
+            return aggregate[0];
+        }
+
+        public override object Next()
+        {
+            object ret = null;
+            if(current < aggregate.items.Count-1)
+            {
+                ret = aggregate[++current];
+            }
+
+            return ret;
+        }
+
+        public override object CurrentItem()
+        {
+            return aggregate[current];
+        }
+
+        public override bool IsDone()
+        {
+            return current >= aggregate.items.Count;
+        }
+    }
+}
