5b2f16ac3e60d60bdcb44ce89ec3c07b56344e2a
Orhan-Veli <orhan_1576@hotmail.com>
2022-07-08 15:29:06 +0300

    Strategy

---
+++ Strategy/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context1 = new Context(new OperationAdd());
+
+            Context context2 = new Context(new OperationSubstract());
+
+            Context context3 = new Context(new OperationMultiply());
+            Console.WriteLine(context1.ExecuteStrategy(10,5));
+            Console.WriteLine(context2.ExecuteStrategy(10,5));
+            Console.WriteLine(context3.ExecuteStrategy(10,5));
+        }
+    }
+
+    public interface IStrategy
+    {
+        int DoOperation(int num1,int num2);
+    }
+
+    public class OperationAdd : IStrategy
+    {
+        public int DoOperation(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+
+    public class OperationSubstract : IStrategy
+    {
+        public int DoOperation(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+
+    public class OperationMultiply : IStrategy
+    {
+        public int DoOperation(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+    }
+
+    public class Context
+    {
+        private IStrategy strategy;
+
+        public Context(IStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public int ExecuteStrategy(int num1,int num2)
+        {
+            return strategy.DoOperation(num1,num2);
+        }
+
+    }
+}
