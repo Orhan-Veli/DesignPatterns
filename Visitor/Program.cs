656ddf29d586828bcf09fd50c187888502584aca
Orhan-Veli <orhan_1576@hotmail.com>
2022-07-08 16:20:36 +0300

    Visitor

---
+++ Visitor/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Visitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IComputerPart computer = new Computer();
+            computer.Accept(new ComputerPartDisplayVisitor());
+        }
+    }
+
+    public interface IComputerPart
+    {
+        void Accept(IComputerPartVisitor computerPartVisitor);
+    }
+
+    public class Keyboard : IComputerPart
+    {
+        public void Accept(IComputerPartVisitor computerPartVisitor)
+        {
+            computerPartVisitor.Visit(this);
+        }
+    }
+
+    public class Monitor : IComputerPart
+    {
+        public void Accept(IComputerPartVisitor computerPartVisitor)
+        {
+            computerPartVisitor.Visit(this);
+        }
+    }
+
+    public class Mouse : IComputerPart
+    {
+        public void Accept(IComputerPartVisitor computerPartVisitor)
+        {
+            computerPartVisitor.Visit(this);
+        }
+    }
+
+    public class Computer : IComputerPart
+    {
+        IComputerPart[] parts;
+
+        public Computer()
+        {
+            parts = new IComputerPart[]{new Mouse(), new Keyboard(),new Monitor()};
+        }
+
+        public void Accept(IComputerPartVisitor computerPartVisitor)
+        {
+            for (int i = 0; i < parts.Length; i++)
+            {
+                parts[i].Accept(computerPartVisitor);
+            }
+            computerPartVisitor.Visit(this);
+        }
+    }
+
+    public interface IComputerPartVisitor
+    {
+        void Visit(Computer computer);
+        void Visit(Mouse mouse);
+        void Visit(Keyboard keyboard);
+        void Visit(Monitor monitor);
+    }
+
+    public class ComputerPartDisplayVisitor : IComputerPartVisitor
+    {
+        public void Visit(Computer computer)
+        {
+            Console.WriteLine("Display comp");
+        }
+
+        public void Visit(Mouse mouse)
+        {
+            Console.WriteLine("Display mouse");
+        }
+
+        public void Visit(Keyboard keyboard)
+        {
+            Console.WriteLine("Displayin keyboard.");
+        }
+
+        public void Visit(Monitor monitor)
+        {
+            Console.WriteLine("Displaying monitor");
+        }
+    }
+
+}
