5c0af5c519820fb601f60a30017a2ae24537f9f9
Orhan-Veli <orhan_1576@hotmail.com>
2022-07-08 23:45:13 +0300

    FrontController

---
+++ FrontController/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace FrontController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FrontController frontController = new FrontController();
+            frontController.DispatchRequest("HOME");
+            frontController.DispatchRequest("STUDENT");
+        }
+    }
+
+    public class HomeView
+    {
+        public void Show()
+        {
+            Console.WriteLine("Displaying HomePage");
+        }
+    }
+
+    public class StudentView
+    {
+        public void Show()
+        {
+            Console.WriteLine("Displaying student page");
+        }
+    }
+
+    public class Dispatcher
+    {
+        public StudentView StudentView { get; set; }
+        public HomeView HomeView { get; set; }
+
+        public Dispatcher()
+        {
+            StudentView = new StudentView();
+            HomeView = new HomeView();
+        }
+
+        public void Dispatch(string request)
+        {
+            if(request == "STUDENT") StudentView.Show();
+            else HomeView.Show();
+        }
+    }
+
+    public class FrontController
+    {
+        public Dispatcher Dispatcher { get; set; }
+
+        public FrontController()
+        {
+            Dispatcher = new Dispatcher();
+        }
+
+        private bool IsAuthenticUser()
+        {
+            Console.WriteLine("User auth");
+            return true;
+        }
+
+        private void TrackRequest(string request)
+        {
+            Console.WriteLine("Page request" + request);
+        }
+
+        public void DispatchRequest(string request)
+        {
+            this.TrackRequest(request);
+
+            if(IsAuthenticUser())
+            {
+                Dispatcher.Dispatch(request);
+            }
+        }
+    }
+}
